f4f96e80f4b76b0cf397ad6e47028adb1aa05ff8
Vaibhav Singh <vaibhavsingh@Vaibhavs-MbPro.local>
2026-08-04 13:30:40 -0700

    Added ssh icons

--- home.nix
+++ home.nix
@@ -44,7 +44,7 @@ in
     enable = true;
     settings = {
       add_newline = false;
-      format = "$directory$git_branch$git_status$cmd_duration$line_break$character";
+      format = "$directory$hostname$git_branch$git_status$cmd_duration$line_break$character";
       character = {
         success_symbol = "[❯](purple)";
         error_symbol = "[❯](red)";
